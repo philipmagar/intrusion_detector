98d983678ed821ee5475de0eec3398d734e23c6f
philipmagar <philipmagar111@gmail.com>
2026-04-18 16:02:27 +0000

    detect attacks

--- sniffer.py
+++ sniffer.py
@@ -1,8 +1,37 @@
-from scapy.all import sniff
+from scapy.all import sniff, IP, TCP
+from collections import defaultdict
+import time
+
+# Store IP activity
+ip_activity = defaultdict(list)
+
+THRESHOLD = 10  # number of ports
+TIME_WINDOW = 10  # seconds
+
+
+def detect_port_scan(ip):
+    ports = ip_activity[ip]
+    
+    # Remove old timestamps
+    current_time = time.time()
+    ip_activity[ip] = [t for t in ports if current_time - t < TIME_WINDOW]
+
+    if len(ip_activity[ip]) > THRESHOLD:
+        print(f"[ALERT] Possible port scan detected from {ip}")
+
 
 def process_packet(packet):
-	print(packet.summary())
+    if packet.haslayer(IP) and packet.haslayer(TCP):
+        src_ip = packet[IP].src
+        
+        # Record timestamp
+        ip_activity[src_ip].append(time.time())
 
-print("starting packet capture ....")
+        detect_port_scan(src_ip)
 
-sniff(prn=process_packet ,store =False)
+        print(packet.summary())
+
+
+print("Starting IDS...")
+
+sniff(prn=process_packet, store=False)
