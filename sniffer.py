e3d63ab1d9d18dcdf02b12862e73c48184e98cf6
philipmagar <philipmagar111@gmail.com>
2026-04-19 13:27:20 +0000

    system that detects port scanning and brute-force behavior

--- sniffer.py
+++ sniffer.py
@@ -1,33 +1,52 @@
+import json
 from scapy.all import sniff, IP, TCP
 from collections import defaultdict
 import time
 
 # Store IP activity
 ip_activity = defaultdict(list)
+connection_attempts = defaultdict(int)
 
-THRESHOLD = 10  # number of ports
-TIME_WINDOW = 10  # seconds
+THRESHOLD = 10
+TIME_WINDOW = 10
+
+
+def log_alert(ip, alert_type):
+    log_entry = {
+        "ip": ip,
+        "type": alert_type,
+        "time": time.strftime("%Y-%m-%d %H:%M:%S")
+    }
+
+    with open("alerts.json", "a") as f:
+        f.write(json.dumps(log_entry) + "\n")
 
 
 def detect_port_scan(ip):
     ports = ip_activity[ip]
-    
-    # Remove old timestamps
+
     current_time = time.time()
     ip_activity[ip] = [t for t in ports if current_time - t < TIME_WINDOW]
 
     if len(ip_activity[ip]) > THRESHOLD:
-        print(f"[ALERT] Possible port scan detected from {ip}")
+        print(f"[ALERT] Port scan detected from {ip}")
+        log_alert(ip, "Port Scan")
 
 
 def process_packet(packet):
     if packet.haslayer(IP) and packet.haslayer(TCP):
         src_ip = packet[IP].src
-        
-        # Record timestamp
-        ip_activity[src_ip].append(time.time())
 
+        # Port scan tracking
+        ip_activity[src_ip].append(time.time())
         detect_port_scan(src_ip)
+
+        # Brute force detection
+        connection_attempts[src_ip] += 1
+
+        if connection_attempts[src_ip] > 20:
+            print(f"[ALERT] Possible brute-force attack from {src_ip}")
+            log_alert(src_ip, "Brute Force")
 
         print(packet.summary())
 
